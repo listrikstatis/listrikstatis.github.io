d8d8908ab5184ed88f448e1071948f64a89828d2
listrikstatis <mediaskripsi1@gmail.com>
2021-02-02 10:39:47 +0800

    first commit

--- js/materi4.js
+++ js/materi4.js
@@ -1,75 +1,66 @@
 //SOAL 1
 function cek(){
+  
   let a1= document.getElementById("1aa").value;
   let b1= document.getElementById("1ab").value;
-  let c1= document.getElementById("1ac").value;
   
-  if (a1 == "9" && b1 == "30" && c1 == "270") {
-    document.getElementsByClassName('cek')[0].style.display='none';
+  if (a1 == "9" && b1 == "30") {
     document.getElementsByClassName('p1')[0].style.display='block';
-    document.getElementsByClassName('j1')[0].style.display='block';
     document.getElementsByClassName('coba')[0].style.display='none';
-  } else {
-     document.getElementById("1aa").style="border: 3px solid red";
-     document.getElementById("1ab").style="border: 3px solid red";   
-     document.getElementById("1ac").style="border: 3px solid red";    
-  }
-
-  if (a1 == "9") {
+    document.getElementsByClassName('jj')[0].style.display='block';
+  } else if(a1=="9"){    
     document.getElementById("1aa").style="border: 3px solid green";
-  } else {
-     document.getElementById("1aa").style="border: 3px solid red";
-  }
-
-  if (b1 == "30") {
+  } else if(b1=="30"){    
     document.getElementById("1ab").style="border: 3px solid green";
-  } else {
-     document.getElementById("1ab").style="border: 3px solid red";
-  }
-
-  if (c1 == "270") {
-    document.getElementById("1ac").style="border: 3px solid green";
-  } else {
-     document.getElementById("1ac").style="border: 3px solid red";
+  } else{    
+    document.getElementById("1aa").style="border: 3px solid red";
+    document.getElementById("1ab").style="border: 3px solid red";
   }
 
 }
 
+function cekk(){
+  let c1= document.getElementById("1ac").value;
+
+  if(c1 == "270"){    
+    document.getElementById("1ac").style="border: 3px solid green";    
+    document.getElementsByClassName('jj')[0].style.display='none';
+    document.getElementsByClassName('ja1')[0].style.display='block';
+  } else{    
+    document.getElementById("1ac").style="border: 3px solid red";
+  }
+}
+
 //SOAL 2
 function cek2(){
+  
   let a2= document.getElementById("2aa").value;
   let b2= document.getElementById("2ab").value;
-  let c2= document.getElementById("2ac").value;
   
-  if (a2 == "45" && b2 == "15" && c2 == "3") {
-    document.getElementsByClassName('cek2')[0].style.display='none';
-    document.getElementsByClassName('p')[0].style.display='block';
-    document.getElementsByClassName('j2')[0].style.display='block';
+  if (a2 == "45" && b2 == "15") {
+    document.getElementsByClassName('p2')[0].style.display='block';
     document.getElementsByClassName('table2')[0].style.display='none';
-  } else {
-     document.getElementById("2aa").style="border: 3px solid red";
-     document.getElementById("2ab").style="border: 3px solid red";   
-     document.getElementById("2ac").style="border: 3px solid red";    
-  }
-
-  if (a2 == "45") {
+    document.getElementsByClassName('jj2')[0].style.display='block';
+  } else if(a2=="45"){    
     document.getElementById("2aa").style="border: 3px solid green";
-  } else {
-     document.getElementById("2aa").style="border: 3px solid red";
-  }
-
-  if (b2 == "15") {
+  } else if(b2=="15"){    
     document.getElementById("2ab").style="border: 3px solid green";
-  } else {
-     document.getElementById("2ab").style="border: 3px solid red";
+  } else{    
+    document.getElementById("2aa").style="border: 3px solid red";
+    document.getElementById("2ab").style="border: 3px solid red";
   }
+}
 
-  if (c2 == "3") {
-    document.getElementById("2ac").style="border: 3px solid green";
-  } else {
-     document.getElementById("2ac").style="border: 3px solid red";
+function cekk2(){
+  let c2= document.getElementById("2ac").value;
+
+  if(c2 == "3"){    
+    document.getElementById("2ac").style="border: 3px solid green";    
+    document.getElementsByClassName('jj2')[0].style.display='none';
+    document.getElementsByClassName('ja2')[0].style.display='block';
+  } else{    
+    document.getElementById("2ac").style="border: 3px solid red";
   }
-
 }
 
 //SOAL 3
