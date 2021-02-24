d4325cc046d5335929cd7b1d6026ef5a9f4f8537
listrikstatis <mediaskripsi1@gmail.com>
2021-02-24 23:23:20 +0800

    first commit

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,32 @@
-function ganti1() {
-	document.getElementById("img").src="img/home.png";
-}
+// function ganti1() {
+// 	document.getElementById("img").src="img/home.png";
+// }
 
-function ganti2(){
-	document.getElementById("img").src="img/kd.png";
-}
+// function ganti2(){
+// 	document.getElementById("img").src="img/kd.png";
+// }
 
-function ganti3(){
-	document.getElementById("img").src="img/petunjuk.png";
-}
-
+// function ganti3(){
+// 	document.getElementById("img").src="img/petunjuk.png";
+// }
 
 $(document).ready(function(){
-		$('.pilih-menu').click(function(){
-			var menu = $(this).attr('id');
-			if(menu == "home"){
-				$('.halaman').load('menu/home.html');												
-			}else if(menu == "kd"){
-				$('.halaman').load('menu/kikd.html');												
-			}else if(menu == "info"){
-				$('.halaman').load('menu/petunjuk.html');											
-			}
-		});
-		// halaman default pertama kali
-		$('.halaman').load('menu/home.html');						
+    $(".hal2").hide();
+    $(".hal3").hide();
+          $(".no1").click(function(){
+                $(".hal2").hide(0);
+                $(".hal3").hide(0);
+                $(".hal1").show(0);
+          });
+          $(".no2").click(function(){
+                $(".hal2").show(0);
+                $(".hal1").hide(0);
+                $(".hal3").hide(0);
+          });
+          $(".no3").click(function(){
+                $(".hal3").show(0);
+                $(".hal2").hide(0);
+                $(".hal1").hide(0);
+          });
+});
 
-	});
