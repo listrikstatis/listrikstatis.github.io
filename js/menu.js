6f74cc4f6a26bd790776d3773394038b5123bdb1
listrikstatis <mediaskripsi1@gmail.com>
2021-03-25 20:49:59 +0800

    first commit

--- js/menu.js
+++ js/menu.js
@@ -17,16 +17,25 @@ $(document).ready(function(){
                 $(".hal2").hide(0);
                 $(".hal3").hide(0);
                 $(".hal1").show(0);
+                $(".no1").addClass("active");
+                $(".no2").removeClass("active");
+                $(".no3").removeClass("active");
           });
           $(".no2").click(function(){
                 $(".hal2").show(0);
                 $(".hal1").hide(0);
                 $(".hal3").hide(0);
+                $(".no2").addClass("active");
+                $(".no1").removeClass("active");
+                $(".no3").removeClass("active");
           });
           $(".no3").click(function(){
                 $(".hal3").show(0);
                 $(".hal2").hide(0);
                 $(".hal1").hide(0);
+                $(".no3").addClass("active");
+                $(".no2").removeClass("active");
+                $(".no1").removeClass("active");
           });
 });
 
