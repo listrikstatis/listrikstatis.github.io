adebc3680ce9c0b5ecc3b20bb9be04c3ee750321
listrikstatis <mediaskripsi@gmail.com>
2021-05-19 22:17:52 +0800

    first commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAZDp9rrxItslIYMXyU2_7Y-eCr1sM63Qo",
-    authDomain: "listrikstatis-b91fe.firebaseapp.com",
-    projectId: "listrikstatis-b91fe",
-    storageBucket: "listrikstatis-b91fe.appspot.com",
-    messagingSenderId: "261164154927",
-    appId: "1:261164154927:web:a273f977e8f6ef428246da"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyCyS5D96kfPKK1qegc0JXvbCdIhu5oGK0E",
+    authDomain: "hasilbelajarsiswa.firebaseapp.com",
+    projectId: "hasilbelajarsiswa",
+    storageBucket: "hasilbelajarsiswa.appspot.com",
+    messagingSenderId: "963667205364",
+    appId: "1:963667205364:web:f8ddf14b5d1b2460d512a1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 window.load = function () {
 
